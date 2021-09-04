6d8d7709805be1c20c4a042742af51da759595cd
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-09-04 17:01:44 -0300

    🚪 set env port

--- api/config/variables.py
+++ api/config/variables.py
@@ -2,5 +2,6 @@ import os
 
 variables_enviroment = {
     "mongo_uri": os.environ.get('mongo_uri'),
-    "postgres_uri": os.environ.get('postgres_uri')
+    "postgres_uri": os.environ.get('postgres_uri'),
+    "port": os.environ.get('port')
 }
--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
+import uvicorn
 from fastapi import FastAPI, APIRouter
 from .controller.questions.controller import questions
 from .controller.users.controller import users
-
+from .config.variables import variables_enviroment
 
 app = FastAPI()
 
 app.include_router(questions)
 app.include_router(users)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port= variables_enviroment['port'] | 5000)
