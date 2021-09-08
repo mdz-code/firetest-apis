22b8ba3d0f252eda96a8ce97e27dfadc2718b0e9
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-09-08 19:46:02 -0300

    👷 cors

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, APIRouter
 from .controller.questions.controller import questions
 from .controller.users.controller import users
@@ -6,8 +7,29 @@ from .config.variables import variables_enviroment
 
 app = FastAPI()
 
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
 app.include_router(questions)
 app.include_router(users)
+
+
+
+
+
 
 
 if __name__ == "__main__":
